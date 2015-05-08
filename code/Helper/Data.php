59f75080ded48934fd937e07969d0e034de7adb3
lukecollymore <dellasu5>
2015-05-08 23:38:08 +0100

    Updated config path for new field location

--- code/Helper/Data.php
+++ code/Helper/Data.php
@@ -30,9 +30,9 @@ class Develo_Googletrustedstores_Helper_Data extends Mage_Core_Helper_Abstract
 	const CONFIG_PATH_BADGE_CONTAINER               = "develo_googletrustedstores/merchant_data/badge_container";
 	const CONFIG_PATH_LOCALE                        = "develo_googletrustedstores/merchant_data/locale";
 	const CONFIG_PATH_ORDER_NUMBER_OPTION           = "develo_googletrustedstores/merchant_data/order_number_option";
-	const CONFIG_PATH_GOOGLE_BASE_SUBACCOUNT_ID     = "develo_googletrustedstores/merchant_data/google_base_subaccount_id";
-	const CONFIG_PATH_GOOGLE_BASE_COUNTRY           = "develo_googletrustedstores/merchant_data/google_base_country";
-	const CONFIG_PATH_GOOGLE_BASE_LANGUAGE          = "develo_googletrustedstores/merchant_data/google_base_language";
+	const CONFIG_PATH_GOOGLE_BASE_SUBACCOUNT_ID     = "develo_googletrustedstores/googleshopping_fields/google_base_subaccount_id";
+	const CONFIG_PATH_GOOGLE_BASE_COUNTRY           = "develo_googletrustedstores/googleshopping_fields/google_base_country";
+	const CONFIG_PATH_GOOGLE_BASE_LANGUAGE          = "develo_googletrustedstores/googleshopping_fields/google_base_language";
 	const CONFIG_PATH_ORDER_ESTIMATE_SHIPPING_DAYS  = "develo_googletrustedstores/order/estimated_shipping_days";
 	const CONFIG_PATH_ORDER_ESTIMATE_DELIVERY_DAYS  = "develo_googletrustedstores/order/estimated_delivery_days";
 
